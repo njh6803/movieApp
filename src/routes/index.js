4d5cc51a9132541bfc58c7092d9a97df0faf533a
njh6803 <njh6803@gmail.com>
2022-03-15 10:20:31 +0900

    first upload

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './Home'
+import About from './About'
+import Movie from './Movie'
+import NotFound from './NotFound'
+
+export default createRouter({
+  // Hash, History
+  // Hash - https://google.com/#/search
+  history: createWebHashHistory(),
+  scrollBehavior() {
+    return { top: 0 }
+  },
+  // pages
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/movie/:id',
+      component: Movie
+    },
+    {
+      path: '/about',
+      component: About
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
+    }
+  ]
+})
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: '노지형',
+    email: 'njh4803@gmail.com',
+    blog: 'https://velog.io/@njh4803',
+    phone: '+82-10-6667-4803',
+    image: 'src/assets/logo.png'
+  })
+}
